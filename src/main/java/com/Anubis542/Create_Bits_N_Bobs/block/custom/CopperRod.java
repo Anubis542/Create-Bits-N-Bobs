5673fd4d6123c664be183130ece9373f546f3999
Anubis542 <jamesh-h@outlook.com>
2025-06-11 21:46:47 +0100

    custom item commit

---
+++ src/main/java/com/Anubis542/Create_Bits_N_Bobs/block/custom/CopperRod.java
@@ -0,0 +1,57 @@
+package com.Anubis542.Create_Bits_N_Bobs.block.custom;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public abstract class CopperRod extends DirectionalBlock {
+    protected static final float AABB_MIN = 6.0F;
+    protected static final float AABB_MAX = 10.0F;
+    protected static final VoxelShape Y_AXIS_AABB = Block.box(6.0, 0.0, 6.0, 10.0, 16.0, 10.0);
+    protected static final VoxelShape Z_AXIS_AABB = Block.box(6.0, 6.0, 0.0, 10.0, 10.0, 16.0);
+    protected static final VoxelShape X_AXIS_AABB = Block.box(0.0, 6.0, 6.0, 16.0, 10.0, 10.0);
+
+    protected CopperRod(BlockBehaviour.Properties p_154339_) {
+        super(p_154339_);
+    }
+
+    @Override
+    protected abstract MapCodec<? extends net.minecraft.world.level.block.RodBlock> codec();
+
+    @Override
+    protected VoxelShape getShape(BlockState p_154346_, BlockGetter p_154347_, BlockPos p_154348_, CollisionContext p_154349_) {
+        switch (p_154346_.getValue(FACING).getAxis()) {
+            case X:
+            default:
+                return X_AXIS_AABB;
+            case Z:
+                return Z_AXIS_AABB;
+            case Y:
+                return Y_AXIS_AABB;
+        }
+    }
+
+    @Override
+    protected BlockState rotate(BlockState p_154354_, Rotation p_154355_) {
+        return p_154354_.setValue(FACING, p_154355_.rotate(p_154354_.getValue(FACING)));
+    }
+
+    @Override
+    protected BlockState mirror(BlockState p_154351_, Mirror p_154352_) {
+        return p_154351_.setValue(FACING, p_154352_.mirror(p_154351_.getValue(FACING)));
+    }
+
+    @Override
+    protected boolean isPathfindable(BlockState p_154341_, PathComputationType p_154344_) {
+        return false;
+    }
+}
